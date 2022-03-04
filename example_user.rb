1214cf02fa9e62c5919b935a6371a88f0ba60ff4
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-04 15:11:04 +0900

    exercise 4.4.5

--- example_user.rb
+++ example_user.rb
@@ -2,11 +2,20 @@ class User
   attr_accessor :name, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    @first_name = attributes[:name].split(' ').first
+    @last_name = attributes[:name].split(' ').last
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{full_name} <#{@email}>"
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def alphabetical_name
+    "#{@last_name}, #{@first_name}"
   end
 end
