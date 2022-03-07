aebc459b0b01ad0bdfbbb43dd599db8c8bed1027
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-07 15:57:12 +0900

    exercise 6.2.4

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,7 +43,7 @@ class UserTest < ActiveSupport::TestCase
   # 無効な例もテスト
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com ]
+                           foo@bar_baz.com foo@bar+baz.com user@example..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
