a4c172f94bc47056a5ac98cdeb9127fe283af252
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-08 15:44:01 +0900

    9.1.1 remember token

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_074552) do
+ActiveRecord::Schema.define(version: 2022_03_08_063753) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
