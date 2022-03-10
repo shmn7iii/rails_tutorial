4306dafa0f7b401abe313e88f1ff27b79d62a66b
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-10 15:53:25 +0900

    10.4.1 admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # メインのサンプルユーザーを1人作成する
 User.create!(name:  "Example User",
-  email: "example@railstutorial.org",
-  password:              "foobar",
-  password_confirmation: "foobar")
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -10,7 +11,7 @@ name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password)
+             email: email,
+             password:              password,
+             password_confirmation: password)
 end
