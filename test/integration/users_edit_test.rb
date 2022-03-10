c1db97b354c1b59aced44c7c1b0aefc4e0e275a1
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-10 15:03:24 +0900

    exercise 10.2.3

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -35,7 +35,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    # ↓演習 10.2.3
+    # assert_equal session[:forwarding_url], edit_user_path(@user)
     log_in_as(@user)
+    # ↓演習 10.2.3
+    # assert_nil session[:forwarding_url]
     assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
