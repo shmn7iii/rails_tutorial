6ea1c5604bcd70586d40af442fdf835d4b8516d4
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-11 16:58:25 +0900

    13.3.1 access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,19 +52,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                 :password_confirmation)
   end
 
   # beforeアクション
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザーかどうか確認
   def correct_user
