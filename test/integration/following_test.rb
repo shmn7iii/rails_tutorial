da75b684edf598dfc0facf9d807c5c68fc87bca0
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-15 09:27:25 +0900

    exercise 14.3.3

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -52,4 +52,12 @@ class FollowingTest < ActionDispatch::IntegrationTest
       delete relationship_path(relationship), xhr: true
     end
   end
+
+  # 演習14.3.3.1
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
 end
